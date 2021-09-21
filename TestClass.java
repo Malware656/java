d2f440ed595d9ef1d5bc0a9feaf7338b89f6be12
Gokulakrishnan <kgokul656@gmail.com>
2021-09-21 22:20:32 +0530

    Iniital commit

---
+++ TestClass.java
@@ -0,0 +1,26 @@
+public class TestClass {
+    public static void main(String[] args) {
+      String name;
+      int age;
+      name = System.getProperty("userName");
+      
+      try{
+        if(name == null){
+          System.out.println("Name not provided");
+        }
+        else if(System.getProperty("age") == null){
+          System.out.println("Age not provided");
+        }
+        else{
+          age = Integer.parseInt(System.getProperty("age"));
+          System.out.printf("Name : %s \n", name);
+          System.out.printf("Age : %d \n", age);
+        }
+      }
+      catch (NumberFormatException e){
+        System.out.println("Give proper integer as Age");
+      }
+    }
+}
+
+// To Run javac TestClass.java && java -DuserName=someName -Dage=23 TestClass 
